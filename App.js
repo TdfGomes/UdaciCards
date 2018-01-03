80a7e6eac02e7d6a153210560076577351de35a5
Tiago Gomes <t.dfgomes@gmail.com>
2018-01-03 14:06:29 +0000

    style deck item

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
 import React from 'react'
-import { View, Platform } from 'react-native'
+import { View, Platform, StatusBar } from 'react-native'
 import DeckList from './components/DeckList'
 import QuizScreen from './components/QuizScreen'
 import { TabNavigator } from 'react-navigation'
 import { Ionicons } from '@expo/vector-icons'
 import { teal, lightGray, lime } from './utils/colors'
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1}}>
-        <Tabs/>
-      </View>
-    );
-  }
-}
+import { Constants } from 'expo'
 
 const Tabs = TabNavigator({
   Home: {
@@ -31,28 +22,44 @@ const Tabs = TabNavigator({
       tabBarIcon: ({ tintColor }) => <Ionicons name={Platform.OS === 'ios' ? 'ios-help-circle' : 'md-help-circle'} size={30} color={tintColor} />
     },
   },
-}, {
-    animationEnabled:true,
-    tabBarOptions: {
-      activeTintColor: Platform.OS === 'ios' ? teal : lightGray,
-      showIcon:true,
-      indicatorStyle:{
-        backgroundColor: lime
+},{
+  animationEnabled:true,
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? teal : lightGray,
+    showIcon:true,
+    indicatorStyle:{
+      backgroundColor: lime
+    },
+    upperCaseLabel:false,
+    style: {
+      height: Platform.OS === 'ios' ? 55 : 65,
+      backgroundColor: Platform.OS === 'ios' ? lightGray : teal,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
       },
-      upperCaseLabel:false,
-      style: {
-        height: Platform.OS === 'ios' ? 55 : 65,
-        backgroundColor: Platform.OS === 'ios' ? lightGray : teal,
-        shadowColor: 'rgba(0, 0, 0, 0.24)',
-        shadowOffset: {
-          width: 0,
-          height: 3
-        },
-        shadowRadius: 6,
-        shadowOpacity: 1
-      }
+      shadowRadius: 6,
+      shadowOpacity: 1
     }
-  })
+  }
+})
 
+const AppStatusBar = ({ backgroundColor, ...props }) => (
+  <View style={{ backgroundColor, height: Constants.statusBarHeight, marginBottom:35 }}>
+    <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+  </View>
+)
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1}}>
+        <AppStatusBar backgroundColor={teal} barStyle="light-content"/>
+        <Tabs/>
+      </View>
+    );
+  }
+}
 
 
--- components/Deck.js
+++ components/Deck.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
-import { teal, gray } from '../utils/colors';
+import { teal, gray, white, lightGray } from '../utils/colors';
 
 const Deck = (props) => (
   <View style={styles.deck}>
     <Text style={styles.title}>{props.title}</Text>
-    <Text style={styles.cards}>{props.cards}</Text>
+    <Text style={styles.cards}>
+      <Text style={styles.number}>{props.cards}</Text>
+      <Text>Cards</Text>
+    </Text>
   </View>
 )
 
@@ -13,16 +16,38 @@ const styles = StyleSheet.create({
   deck: {
     flex:1,
     justifyContent:'center',
-    alignItems:'center'
+    alignItems:'center',
+    borderRadius:2,
+    margin:25,
+    padding:45,
+    backgroundColor:white,
+    shadowColor: 'rgba(80, 80, 80, 0.34)',
+    shadowOffset: {
+      width: 0,
+      height: 0
+    },
+    shadowRadius: 2,
+    shadowOpacity: 0.5,
+    borderWidth:0.5,
+    borderColor:'#E0E0E0'
   },
   title:{
     color:teal,
     fontWeight:'300',
-    fontSize:20
+    fontSize:20,
+    marginBottom:10
   },
   cards:{
     color:gray,
     fontSize:15
+  },
+  number:{
+    borderRadius:5,
+    borderWidth:0.5,
+    // borderStyle:'solid',
+    borderColor:teal,
+    padding:5,
+    marginRight:3
   }
 })
 
