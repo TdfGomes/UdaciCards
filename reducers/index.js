afcfda813c304e03663d8665ee461622c565443d
Tiago Gomes <t.dfgomes@gmail.com>
2018-01-25 12:05:57 +0000

    redifine redux debugger and add card reducer

--- reducers/index.js
+++ reducers/index.js
@@ -18,6 +18,7 @@ const decks = (state = {}, action) => {
       return {
         ...state,
         [action.deck]:{
+          ...state[action.deck],
           questions,
         }
       }
--- store/index.js
+++ store/index.js
@@ -1,15 +1,10 @@
-import { Platform } from 'react-native';
-import { createStore, applyMiddleware, compose } from 'redux';
-import devTools from 'remote-redux-devtools';
-import decks from '../reducers';
+import { createStore, applyMiddleware, compose } from 'redux'
+import { createLogger } from 'redux-logger'
+import decks from '../reducers'
 
-export default function configureStore(initialState) {
-  const enhancer = compose(
-    devTools({
-      name: Platform.OS,
-      hostname: 'localhost',
-      port: 19001
-    })
-  );
-  return createStore(decks, initialState, enhancer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const loggerMiddleware = createLogger()
+
+export default function configureStore(preloadedState) {
+  return createStore(decks, preloadedState, composeEnhancers(applyMiddleware(loggerMiddleware)));
 }
